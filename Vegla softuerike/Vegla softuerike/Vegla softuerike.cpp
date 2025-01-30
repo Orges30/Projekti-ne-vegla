ff3058966b2620a4621142d46051d804f0fdbe26
Orges30 <orges.gashi2@student.uni-pr.edu>
2025-01-30 19:53:00 +0100

    Fituesit e bursave.
    Kushtet se kush fiton burse nga shteti e kush nga fakulteti.

--- Vegla softuerike/Vegla softuerike/Vegla softuerike.cpp
+++ Vegla softuerike/Vegla softuerike/Vegla softuerike.cpp
@@ -22,24 +22,34 @@ return shuma / NR_LENDEVE;
 }
 
 
-int indeksMaxMesatare = 0;
-for (int i = 1; i <= NR_LENDEVE; i++) {
-    if (studentet[i].mesatarja > studentet[indeksMaxMesatare].mesatarja) {
-        indeksMaxMesatare = i;
-    }
-}
-
-int maxNotaIndex[NR_LENDEVE] = { 0 };
-for (int j = 0; j < NR_LENDEVE; j++) {
-    for (int i = 1; i <= NUM_STUDENTEVE; i++) {
-        if (studentet[i].notat[j] > studentet[maxNotaIndex[j]].notat[j]) {
-            maxNotaIndex[j] = i;
-        }
-    }
-}
-
-
 int main()
 {
-    cout << "Hello World!\n";
+    Student studentet[NUM_STUDENTEVE];
+    int indeksMaxMesatare = 0;
+    for (int i = 1; i <= NR_LENDEVE; i++) {
+        if (studentet[i].mesatarja > studentet[indeksMaxMesatare].mesatarja) {
+            indeksMaxMesatare = i;
+        }
+    }
+
+    int maxNotaIndex[NR_LENDEVE] = { 0 };
+    for (int j = 0; j < NR_LENDEVE; j++) {
+        for (int i = 1; i <= NUM_STUDENTEVE; i++) {
+            if (studentet[i].notat[j] > studentet[maxNotaIndex[j]].notat[j]) {
+                maxNotaIndex[j] = i;
+            }
+        }
+    }
+
+    int bursaFakulteti = 0, bursaShteti = 0;
+    for (int i = 0; i < NUM_STUDENTEVE; i++) {
+        if (studentet[i].mesatarja >= 8.0) {
+            bursaFakulteti++;
+        }
+        if (studentet[i].mesatarja >= 9.0) {
+            bursaShteti++;
+        }
+    }
+
+
 }
