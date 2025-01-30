a6be9c5a25cf77d030e2ec4780a6109b1696fe9c
Orges30 <orges.gashi2@student.uni-pr.edu>
2025-01-30 19:43:07 +0100

    Nota max ne secilen lende
    Gjetja e studentit me noten me te larte ne secilen lende.

--- Vegla softuerike/Vegla softuerike/Vegla softuerike.cpp
+++ Vegla softuerike/Vegla softuerike/Vegla softuerike.cpp
@@ -14,13 +14,22 @@ struct Student {
 };
 
 
-//Gjetja per mesataren me te larte:
 int indeksMaxMesatare = 0;
 for (int i = 1; i <= NR_LENDEVE; i++) {
     if (studentet[i].mesatarja > studentet[indeksMaxMesatare].mesatarja) {
         indeksMaxMesatare = i;
     }
 }
+
+int maxNotaIndex[NR_LENDEVE] = { 0 };
+for (int j = 0; j < NR_LENDEVE; j++) {
+    for (int i = 1; i <= NUM_STUDENTEVE; i++) {
+        if (studentet[i].notat[j] > studentet[maxNotaIndex[j]].notat[j]) {
+            maxNotaIndex[j] = i;
+        }
+    }
+}
+
 
 int main()
 {
